7b79aa18b6792d42f961f438c3fcc03febbb37e2
abhisekguha <abhisekguha2@gmail.com>
2023-10-05 08:32:19 +0530

    modified

--- myApp.js
+++ myApp.js
@@ -1,58 +1,22 @@
 const express = require('express');
 const helmet = require('helmet');
+const api = require('./server.js');
+
 const app = express();
 
+app.use(helmet());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`🦴Using Pogrammer Info Security App Started on Port ${port}`);
+  console.log(`🦴Using Programmer Info Security App Started on Port ${port}`);
 });
 
-app.use(helmet.hidePoweredBy());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = app;
